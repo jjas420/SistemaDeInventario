716c2c1c7cac3f8bbac032be95e05d210966fa5b
jjas420 <ayosuajon4@gmail.com>
2022-12-19 21:21:51 -0300

    Comienzo_de_proyecto_19/12/2022

---
+++ src/main/java/com/mycompany/sistemadeinvenario/SistemaDeInvenario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sistemadeinvenario;
+
+/**
+ *
+ * @author ayosu
+ */
+public class SistemaDeInvenario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       Login  login = new Login ();
+       login.setLocationRelativeTo(null);
+        login.setVisible(true);
+        login.setTitle("login");
+    }
+}
